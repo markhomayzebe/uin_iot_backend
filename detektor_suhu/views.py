5fc51241984149aa448a3b969c5a63136600448d
markhomayzebe <56153845+markhomayzebe@users.noreply.github.com>
2019-12-13 16:33:42 +0700

    django mboh
    Signed-off-by: mac <markhomayzebe99@gmail.com>

--- detektor_suhu/views.py
+++ detektor_suhu/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import
+from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
 from detektor_suhu.models import Stats
-from detektor_suhu.serializers import StartSerializer
+from detektor_suhu.serializers import StartSerializers
 
 class StatsViewSet(viewsets.ModelViewSet):
     queryset = Stats.objects.all()
-    serializer_class = StartSerializer
+    serializer_class = StartSerializers
 
